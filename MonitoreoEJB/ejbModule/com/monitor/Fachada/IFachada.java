18e5c2488a858aa4850e91a2d8143ec26d6c2439
Matias Kreder <mkreder@gmail.com>
2015-10-25 16:49:37 -0300

    mas metodos para IFachada y VODespacho faltaba

--- MonitoreoEJB/ejbModule/com/monitor/Fachada/IFachada.java
+++ MonitoreoEJB/ejbModule/com/monitor/Fachada/IFachada.java
@@ -1,10 +1,12 @@
 package com.monitor.Fachada;
 
+import java.util.ArrayList;
+
 import javax.ejb.Remote;
 
 import dominio.InformeDeAuditoria;
-
 import vos.VoVenta;
+import vos.VoDespacho;
 
 @Remote
 public interface IFachada {
@@ -12,4 +14,8 @@ public interface IFachada {
 	boolean procesarVenta(VoVenta venta);
 
 	void generarInformeAuditoria(InformeDeAuditoria i);
+
+	ArrayList<VoVenta> obtenerVentasSinOrdenesDeDespacho();
+
+	ArrayList<VoDespacho> obtenerDespachosActivos();
 }
